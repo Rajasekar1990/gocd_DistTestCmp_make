f6f55ef18625551784e6a14c3b446d0ab6ad0f0c
rajasekar.s1 <rajasekar.s1@0222LTPB0070525.local>
2022-09-29 17:40:13 +0530

    full pipeline enabled | Loadtest from Master | no slaves

--- CreateHTMLReport.sh
+++ CreateHTMLReport.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
 echo "##Creating HTML Report##"
+#docker exec -i jm_master /bin/bash -c "cd /home/jmeter/ && jmeter -g /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl -e -o /home/jmeter/htmlreport-${GO_PIPELINE_COUNTER}/"
 docker exec -i jm_master /bin/bash -c "cd /home/jmeter/ && jmeter -g /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl -e -o /home/jmeter/htmlreport-${GO_PIPELINE_COUNTER}/"
 
 echo "##Copying JTL and HTML Report to GOCD working DIR##"
 # cd jm
 echo "Current Working DIR is:$PWD"
 echo "Current Working DIR file list:$(ls -lrt)"
-mkdir Report-${GO_PIPELINE_COUNTER}
-echo "Current Working DIR file list with Report dir created:$(ls -lrt)"
+# mkdir Report-${GO_PIPELINE_COUNTER}
+# echo "Current Working DIR file list with Report dir created:$(ls -lrt)"
 
 echo "jm_master Current Working DIR is:$(docker exec -i jm_master /bin/bash -c "pwd && ls -lrt")"
-docker cp jm_master:/home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl ./Report-${GO_PIPELINE_COUNTER}/jmeter-${GO_PIPELINE_COUNTER}.jtl
-docker cp jm_master:/home/jmeter/htmlreport-${GO_PIPELINE_COUNTER}/ ./Report-${GO_PIPELINE_COUNTER}/htmlreport-${GO_PIPELINE_COUNTER}/
+docker cp jm_master:/home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl ./Report-${GO_PIPELINE_COUNTER}.jtl
+docker cp jm_master:/home/jmeter/htmlreport-${GO_PIPELINE_COUNTER}/ ./Report-${GO_PIPELINE_COUNTER}
 
 ##> /dev/null && cat /home/jmeter/jmeter${GO_PIPELINE_COUNTER}.jtl' > test/jmeter${GO_PIPELINE_COUNTER}jtl
 echo "pwd is: $pwd"
 ls -lrt 
 
-echo "Check Copied Report files on GOCD Agent Report DIR"
+echo "Check Copied Report html report files on GOCD Agent htmlreport DIR"
 ls -lrt Report-${GO_PIPELINE_COUNTER}
