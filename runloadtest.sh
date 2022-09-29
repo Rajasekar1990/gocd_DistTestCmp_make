06ef45d6a64b4e2583b214f85387500fd4c2ea48
rajasekar.s1 <rajasekar.s1@0222LTPB0070525.local>
2022-09-29 21:28:42 +0530

    full pipeline enabled | jmeter version 5.3 | Load Injection from Slave machines

--- runloadtest.sh
+++ runloadtest.sh
@@ -82,11 +82,11 @@ docker exec -i jm_master /bin/bash -c "ls -lrt"
 #docker exec -i -e JVM_ARGS="-Xms2048m -Xmx4096m" jm_master /bin/bash -c "cd /home/jmeter/apache-jmeter-5.3/bin && jmeter -n -t /home/jmeter/test/Pipeline_SampleScript.jmx -Dserver.rmi.ssl.disable=true -R${finalipset} -l /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl"
 
 #Run the below command to inject load only from Master
-docker exec -i -e JVM_ARGS="-Xms2048m -Xmx4096m" jm_master /bin/bash -c "cd /home/jmeter/ && jmeter -n -t /home/jmeter/test/Pipeline_SampleScript.jmx -l /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl"
+#docker exec -i -e JVM_ARGS="-Xms2048m -Xmx4096m" jm_master /bin/bash -c "cd /home/jmeter/ && jmeter -n -t /home/jmeter/test/Pipeline_SampleScript.jmx -l /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl"
 
 #Run the below command to inject load using Slave machines
-# echo "jmeter -n -t /home/jmeter/test/Pipeline_SampleScript.jmx -Dserver.rmi.ssl.disable=true -R${finalipset} -l /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl"
-# docker exec -i -e JVM_ARGS="-Xms2048m -Xmx4096m" jm_master /bin/bash -c "cd /home/jmeter/ && jmeter -n -t /home/jmeter/test/Pipeline_SampleScript.jmx -Dserver.rmi.ssl.disable=true -R${finalipset} -l /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl"
+echo "jmeter -n -t /home/jmeter/test/Pipeline_SampleScript.jmx -Dserver.rmi.ssl.disable=true -R${finalipset} -l /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl"
+docker exec -i -e JVM_ARGS="-Xms2048m -Xmx4096m" jm_master /bin/bash -c "cd /home/jmeter/ && jmeter -n -t /home/jmeter/test/Pipeline_SampleScript.jmx -Dserver.rmi.ssl.disable=true -R${finalipset} -l /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl"
 
 # echo "##Docker container logs##"
 # docker logs jm_master
