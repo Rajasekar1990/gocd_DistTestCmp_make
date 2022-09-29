462f44ace57c93b00a4be4173f5244ed502e1a8e
rajasekar.s1 <rajasekar.s1@0222LTPB0070525.local>
2022-09-29 17:54:23 +0530

    full pipeline enabled | Loadtest from Master | no slaves

--- CreateHTMLReport.sh
+++ CreateHTMLReport.sh
@@ -12,12 +12,12 @@ echo "Current Working DIR file list:$(ls -lrt)"
 # echo "Current Working DIR file list with Report dir created:$(ls -lrt)"
 
 echo "jm_master Current Working DIR is:$(docker exec -i jm_master /bin/bash -c "pwd && ls -lrt")"
-docker cp jm_master:/home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl ./Report-${GO_PIPELINE_COUNTER}.jtl
 docker cp jm_master:/home/jmeter/htmlreport-${GO_PIPELINE_COUNTER}/ ./Report-${GO_PIPELINE_COUNTER}
+docker cp jm_master:/home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl ./Report-${GO_PIPELINE_COUNTER}/jmeter-${GO_PIPELINE_COUNTER}.jtl
 
 ##> /dev/null && cat /home/jmeter/jmeter${GO_PIPELINE_COUNTER}.jtl' > test/jmeter${GO_PIPELINE_COUNTER}jtl
 echo "pwd is: $pwd"
 ls -lrt 
 
-echo "Check Copied Report html report files on GOCD Agent htmlreport DIR"
+echo "Check files in $(Report-${GO_PIPELINE_COUNTER})"
 ls -lrt Report-${GO_PIPELINE_COUNTER}
