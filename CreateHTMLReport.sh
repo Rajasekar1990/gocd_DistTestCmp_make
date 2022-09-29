dbe35321244bc509062b384b4047b98ffdf8730d
rajasekar.s1 <rajasekar.s1@0222LTPB0070525.local>
2022-09-29 17:02:57 +0530

    full pipeline enabled | Loadtest from Master | no slaves

--- CreateHTMLReport.sh
+++ CreateHTMLReport.sh
@@ -15,5 +15,8 @@ docker cp jm_master:/home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl ./Report-${GO
 docker cp jm_master:/home/jmeter/htmlreport-${GO_PIPELINE_COUNTER}/ ./Report-${GO_PIPELINE_COUNTER}/htmlreport-${GO_PIPELINE_COUNTER}/
 
 ##> /dev/null && cat /home/jmeter/jmeter${GO_PIPELINE_COUNTER}.jtl' > test/jmeter${GO_PIPELINE_COUNTER}jtl
+echo "pwd is: $pwd"
+ls -lrt 
+
 echo "Check Copied Report files on GOCD Agent Report DIR"
 ls -lrt Report-${GO_PIPELINE_COUNTER}
