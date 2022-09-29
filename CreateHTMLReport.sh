1f7faf91819690dacd06ccd94f62c2b75059a6ca
rajasekar.s1 <rajasekar.s1@0222LTPB0070525.local>
2022-09-29 18:04:30 +0530

    full pipeline enabled | Loadtest from Master | no slaves engaged

--- CreateHTMLReport.sh
+++ CreateHTMLReport.sh
@@ -13,11 +13,11 @@ echo "Current Working DIR file list:$(ls -lrt)"
 
 echo "jm_master Current Working DIR is:$(docker exec -i jm_master /bin/bash -c "pwd && ls -lrt")"
 docker cp jm_master:/home/jmeter/htmlreport-${GO_PIPELINE_COUNTER}/ ./Report-${GO_PIPELINE_COUNTER}
-docker cp jm_master:/home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl ./Report-${GO_PIPELINE_COUNTER}/jmeter-${GO_PIPELINE_COUNTER}.jtl
+docker cp jm_master:/home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl ./Report-${GO_PIPELINE_COUNTER}.jtl
 
 ##> /dev/null && cat /home/jmeter/jmeter${GO_PIPELINE_COUNTER}.jtl' > test/jmeter${GO_PIPELINE_COUNTER}jtl
 echo "pwd is: $pwd"
 ls -lrt 
 
-echo "Check files in $(Report-${GO_PIPELINE_COUNTER})"
+echo "Check files in Report-${GO_PIPELINE_COUNTER} folder"
 ls -lrt Report-${GO_PIPELINE_COUNTER}
