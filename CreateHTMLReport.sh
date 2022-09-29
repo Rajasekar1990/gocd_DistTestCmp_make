50d6c44cb032ca5dc49594720397f8f7286a19a0
rajasekar.s1 <rajasekar.s1@dctestorautw.idfcbank.com>
2022-09-29 14:18:44 +0530

    full pipeline enabled | Loadtest from Master | no slaves

--- CreateHTMLReport.sh
+++ CreateHTMLReport.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "##Creating HTML Report##"
-docker exec -i jm_master /bin/bash -c 'cd /home/jmeter/ && jmeter -g /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl -e -o /home/jmeter/htmlreport${GO_PIPELINE_COUNTER}/'
+docker exec -i jm_master /bin/bash -c "cd /home/jmeter/ && jmeter -g /home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl -e -o /home/jmeter/htmlreport${GO_PIPELINE_COUNTER}/"
 
 echo "##Copying JTL and HTML Report to GOCD working DIR##"
-cd jm
+# cd jm
 echo "Current Working DIR is:$PWD"
 docker cp jm_master:/home/jmeter/jmeter-${GO_PIPELINE_COUNTER}.jtl test/jmeter-${GO_PIPELINE_COUNTER}.jtl
 docker cp jm_master:/home/jmeter/htmlreport${GO_PIPELINE_COUNTER}/ test/jmeter-htmlreport${GO_PIPELINE_COUNTER}/
