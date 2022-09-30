5ded067473627beb6d4ee75247a94f4066ead38a
rajasekar.s1 <rajasekar.s1@0222LTPB0070525.local>
2022-09-30 17:56:15 +0530

    gocd yml | master and slave sh files updated | Fullpipeline enabled

--- ACR_Build_Push_Master_Img.sh
+++ ACR_Build_Push_Master_Img.sh
@@ -5,15 +5,15 @@ echo "listing files in current working dir"
 ls -lrt
 
 echo "Build Jmeter Master Image"
-Master_Build=$(docker build -f jmetermasterimage/Dockerfile -t jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azmastergocd . | grep "writing image" | wc -l)
+docker build -f jmetermasterimage/Dockerfile -t jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azmastergocd .
+buildstatus=$?
 
-echo "Master_BuildStatus:$Master_Build"
-if [ $Master_Build != 0 ] 
+if [ $buildstatus -eq 0 ] 
 then
     echo "Push JMeter Master Image to ACR"
-    MasterImg_Push=$(docker push jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azmastergocd | grep "digest" | wc -l)
-    echo "MasterImg_PushStatus:$MasterImg_Push"
-    if [ $MasterImg_Push != 0 ] 
+    docker push jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azmastergocd
+    pushstatus=$?
+    if [ $pushstatus -eq 0 ] 
     then
         echo "Jmeter Master Image Push Successful"
     else
--- ACR_Build_Push_Slave_Img.sh
+++ ACR_Build_Push_Slave_Img.sh
@@ -5,15 +5,15 @@ echo "listing files in current working dir"
 ls -lrt
 
 echo "Build Jmeter Slave Image"
-Slave_Build=$(docker build -f jmeterslaveimage/Dockerfile -t jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azslavegocd . | grep "writing image" | wc -l)
-echo "Slave_BuildStatus:$Slave_Build"
+docker build -f jmeterslaveimage/Dockerfile -t jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azslavegocd .
+buildstatus=$?
 
-if [ $Slave_Build != 0 ] 
+if [ $buildstatus -eq 0 ] 
 then
     echo "Push JMeter Slave Image to ACR"
-    SlaveImg_Push=$(docker push jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azslavegocd | grep "digest" | wc -l)
-    echo "SlaveImg_PushStatus:$SlaveImg_Push"
-    if [ $SlaveImg_Push != 0 ] 
+    docker push jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azslavegocd
+    pushstatus=$?
+    if [ $pushstatus -eq 0 ] 
     then
         echo "Jmeter Slave Image Push Successful"
     else
