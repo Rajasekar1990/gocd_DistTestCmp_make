902caa23d9046cbeef3f40914d4482231ba08511
rajasekar.s1 <rajasekar.s1@0222LTPB0070525.local>
2022-09-30 17:27:07 +0530

    gocd yml | master and slave sh files updated

--- ACR_Build_Push_Master_Img.sh
+++ ACR_Build_Push_Master_Img.sh
@@ -5,13 +5,13 @@ echo "listing files in current working dir"
 ls -lrt
 
 echo "Build Jmeter Master Image"
-Master_Build=$(docker build -f jmetermasterimage/Dockerfile -t jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azmastergocd . | grep "Successfully built" | wc -l)
+Master_Build=$(docker build -f jmetermasterimage/Dockerfile -t jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azmastergocd . | grep "naming to jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azmastergocd done" | wc -l)
 
-if [$Master_Build != 0] 
+if [ $Master_Build != 0 ] 
 then
     echo "Push JMeter Master Image to ACR"
     MasterImg_Push=$(docker push jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azmastergocd | grep "jmeter5.3Azmastergocd" | wc -l)
-    if [$MasterImg_Push != 0] 
+    if [ $MasterImg_Push != 0 ] 
     then
         echo "Jmeter Master Image Push Successful"
     else
--- ACR_Build_Push_Slave_Img.sh
+++ ACR_Build_Push_Slave_Img.sh
@@ -5,13 +5,13 @@ echo "listing files in current working dir"
 ls -lrt
 
 echo "Build Jmeter Slave Image"
-Slave_Build=$(docker build -f jmetermasterimage/Dockerfile -t jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azslavegocd . | grep "Successfully built" | wc -l)
+Slave_Build=$(docker build -f jmetermasterimage/Dockerfile -t jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azslavegocd . | grep "naming to jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azslavegocd done" | wc -l)
 
-if [$Slave_Build != 0] 
+if [ $Slave_Build != 0 ] 
 then
     echo "Push JMeter Slave Image to ACR"
     SlaveImg_Push=$(docker push jmeteracrrepo.azurecr.io/jmeter:jmeter5.3Azslavegocd | grep "jmeter5.3Azslavegocd" | wc -l)
-    if [$SlaveImg_Push != 0] 
+    if [ $SlaveImg_Push != 0 ] 
     then
         echo "Jmeter Slave Image Push Successful"
     else
